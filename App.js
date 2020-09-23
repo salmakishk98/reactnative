56ab742daefb9e2b883888e614acbcb38cae0ea6
salmakishk98 <salma.mohamed2255@gmail.com>
2020-09-23 22:50:46 +0200

    pactise code in react native

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Button,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  // console.log(useDimensions());
+  const { landscape } = useDeviceOrientation();
+  console.log(useDeviceOrientation());
+  const handlepressevent = () => {
+    console.log("pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlepressevent}>
+        {"  "}
+        this is mobile app{" "}
+      </Text>
+      <TouchableOpacity onPress={() => console.log("touched")}>
+        <Image
+          //blurRadius={4}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/id/1/200/300",
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableNativeFeedback onPress={() => console.log("hello")}>
+        <View
+          style={{
+            width: landscape ? "100%" : "25%",
+            height: "25%",
+            backgroundColor: "yellow",
+          }}
+        ></View>
+      </TouchableNativeFeedback>
+
+      <Button
+        title="ClickMe"
+        onPress={() => alert("are you sure")}
+        color="black"
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    //make component in center
+    //alignItems: "center",
+    //justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
